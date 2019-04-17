72d332f284fcbcfa7d9a2af2ea0b7d4e300f4f1b
Trash Panda <d.george.schlosser@gmail.com>
2019-04-17 01:04:01 -0400

    initial button generator

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,23 @@
 // ==========================================================
 
 // mythological beasts
-var topics = [dragon, pheonix, kraken, sphinx, basilisk, manticore, leviathon, cthullu];
+var topics = ["dragon", "pheonix", "kraken", "sphinx", "basilisk", "manticore", "leviathon", "cthullu"];
 
 
 // FUNCTIONS
 // ==========================================================
 
 // create buttons using for-loop for each string in array
+function generateButtons() {
+    $("#button-dump").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var b = $("<button>");
+        b.addClass("beast-btn");
+        b.attr("data-name", topics[i]);
+        b.text(topics[i]);
+        $("#button-dump").append(b);
+    }
+};
 
 // take user input from html form and add to topics
     // recreate buttons
@@ -18,6 +28,7 @@ var topics = [dragon, pheonix, kraken, sphinx, basilisk, manticore, leviathon, c
 // APPLICATION
 // ==========================================================
 
+generateButtons();
 // when button pressed, grab 10 paused gifs from giphy api
     // when gif clicked play, click again to pause
 // display rating under gif
