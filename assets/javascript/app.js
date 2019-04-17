a236fc4ff715de083cce6a7b289a0138f579db22
Trash Panda <d.george.schlosser@gmail.com>
2019-04-17 00:41:31 -0400

    psuedocode, var topics made

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// GLOBAL VARIABLES
+// ==========================================================
+
+// mythological beasts
+var topics = [dragon, pheonix, kraken, sphinx, basilisk, manticore, leviathon, cthullu];
+
+
+// FUNCTIONS
+// ==========================================================
+
+// create buttons using for-loop for each string in array
+
+// take user input from html form and add to topics
+    // recreate buttons
+
+
+
+// APPLICATION
+// ==========================================================
+
+// when button pressed, grab 10 paused gifs from giphy api
+    // when gif clicked play, click again to pause
+// display rating under gif
+
